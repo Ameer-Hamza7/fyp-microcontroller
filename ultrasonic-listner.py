7351582cd77a28c64629b15d71ef32624ed351b3
Ameer-Hamza7 <91970090+Ameer-Hamza7@users.noreply.github.com>
2024-01-07 01:56:03 +0500

    Adding 4 Bins Sensors

--- ultrasonic-listner.py
+++ ultrasonic-listner.py
@@ -2,15 +2,15 @@ import RPi.GPIO as GPIO
 import time
 
 # Define GPIO pins
-TRIG_PIN = 17
-ECHO_PIN = 18
+# TRIG_PIN = 16
+# ECHO_PIN = 32
 
-def setup():
-    GPIO.setmode(GPIO.BCM)
+def setup_bin(TRIG_PIN, ECHO_PIN):
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(TRIG_PIN, GPIO.OUT)
     GPIO.setup(ECHO_PIN, GPIO.IN)
-
-def get_distance():
+    
+def get_bin_level(TRIG_PIN, ECHO_PIN):
     GPIO.output(TRIG_PIN, True)
     time.sleep(0.00001)
     GPIO.output(TRIG_PIN, False)
@@ -34,10 +34,19 @@ def cleanup():
 
 if __name__ == "__main__":
     try:
-        setup()
+        setup_bin(TRIG_PIN=16, ECHO_PIN=32)
+        setup_bin(TRIG_PIN=18, ECHO_PIN=36)
+        setup_bin(TRIG_PIN=22, ECHO_PIN=38)
+        setup_bin(TRIG_PIN=24, ECHO_PIN=40)
         while True:
-            distance = get_distance()
-            print(f"Distance: {distance:.2f} cm")
+            distance1 = get_bin_level(TRIG_PIN=16, ECHO_PIN=32)
+            distance2 = get_bin_level(TRIG_PIN=18, ECHO_PIN=36)
+            distance3 = get_bin_level(TRIG_PIN=22, ECHO_PIN=38)
+            distance4 = get_bin_level(TRIG_PIN=24, ECHO_PIN=40)
+            print(f"Distance: {distance1:.2f} cm")
+            print(f"Distance: {distance2:.2f} cm")
+            print(f"Distance: {distance3:.2f} cm")
+            print(f"Distance: {distance4:.2f} cm")
             time.sleep(1)
 
     except KeyboardInterrupt:
