c19a3f36ccaf9d55b76d1e3ba491995975846cc1
Ameer-Hamza7 <91970090+Ameer-Hamza7@users.noreply.github.com>
2024-01-11 02:05:43 +0500

    Setting up Python Module

--- ir_listener.py
+++ ir_listener.py
@@ -6,17 +6,26 @@ eye_pin_1 = 13
 eye_pin_2 = 15
 
 # Setup GPIO mode and configure the pins as input
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(eye_pin_1, GPIO.IN)
-GPIO.setup(eye_pin_2, GPIO.IN)
+
+def eye_setup(pin_no):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin_no, GPIO.IN)
+
+    return True
+
+def see(pin_no):
+    value = GPIO.input(pin_no)
+    
+    return value
+
+eye_setup(pin_no=eye_pin_1)
+eye_setup(pin_no=eye_pin_2)
 
 try:
     while True:
         # Read data from sensor 1
-        eye_1_value = GPIO.input(eye_pin_1)
-        
-        # Read data from sensor 2
-        eye_2_value = GPIO.input(eye_pin_2)
+        eye_1_value = see(pin_no=eye_pin_1)
+        eye_2_value = see(pin_no=eye_pin_2)
 
         # Print the sensor values
         print(f"Sensor 1: {eye_1_value}, Sensor 2: {eye_2_value}")
---
+++ power-on-self-test.py
@@ -0,0 +1,27 @@
+from .ultrasonic_listener import setup_bin, get_bin_level
+from .ir_listener import eye_setup, see
+import RPi.GPIO as GPIO
+
+
+if __name__ == "__main__":
+    setup_bin(TRIG_PIN=16, ECHO_PIN=40)
+    setup_bin(TRIG_PIN=18, ECHO_PIN=32)
+    setup_bin(TRIG_PIN=22, ECHO_PIN=36)
+    
+    bin1_chk = get_bin_level(TRIG_PIN=16, ECHO_PIN=40)
+    bin2_chk = get_bin_level(TRIG_PIN=18, ECHO_PIN=32)
+    bin3_chk = get_bin_level(TRIG_PIN=22, ECHO_PIN=36)
+    
+    if bin1_chk is not None and bin2_chk is not None and bin3_chk is not None:
+        print("bin 01 status - OK \n bin 02 status - OK \n bin 03 status - OK ")
+        
+    eye_setup(pin_no=13)
+    eye_setup(pin_no=15)
+
+    val1 = see(pin_no=13) 
+    val2 = see(pin_no=15) 
+
+    if val1 is not None and val2:
+        print("eye 01 status - OK \n eye 02 status - OK")
+    
+    print("Everything Works fine")
